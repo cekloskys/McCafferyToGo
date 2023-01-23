dd772ee963503cf01eece6cc7c6c1e22232ac454
Saikoyomii9 <saikoyomii9@gmail.com>
2023-01-23 15:35:25 -0500

    Orders and Order Details Screens

--- src/screens/OrdersScreen/index.js
+++ src/screens/OrdersScreen/index.js
@@ -1,13 +1,20 @@
-import { View } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import styles from './styles';
+import Order from '../../components/Order';
+import orders from '../../../assets/data/orders.json';
 
 const OrdersScreen = () => {
 
     return (
-        <View style={styles.page}>
-          
+      
+        <View style={{flex: 1, width: '100%'}}>
+          <Text style={{textDecoration: 'solid', color: 'red', textAlign: 'center', marginVertical: 50, fontSize: 25}}>Welcome To Your Orders!</Text>
+          <FlatList
+          data={orders}
+          renderItem={({item}) => <Order order={item}/>}
+          />
         </View>
       );
-}
+};
 
 export default OrdersScreen;
