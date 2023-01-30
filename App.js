049376250a676d60e2377ddfc5bab434e9b1cc57
Saikoyomii9 <saikoyomii9@gmail.com>
2023-01-30 13:26:41 -0500

    January 30 2023

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+Amplify.configure({...awsconfig, Analytics: {disabled: true}});
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -11,3 +16,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
