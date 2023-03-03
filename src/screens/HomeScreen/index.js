9edbed3a74c8196d9968d16229a5d2f782ce9b2a
Saikoyomii9 <saikoyomii9@gmail.com>
2023-03-03 15:58:21 -0500

    3/3/2023

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -3,10 +3,15 @@ import RestaurantItem from '../../components/RestaurantItem';
 import styles from './styles';
 import { useState, useEffect } from 'react';
 import { DataStore } from 'aws-amplify';
-import { Restaurant } from '../../models';
+import { Restaurant, User } from '../../models';
+import { useAuthContext } from '../../context/AuthContext';
+import {ALERT_TYPE, Dialog, Root} from "react-native-alert-notification";
+
 
 
 export default function HomeScreen() {
+
+  const { dbUser } = useAuthContext()
   const [restaurants, setRestaurants] = useState([]);
 
   const fetchRestaurants = async () => {
@@ -19,14 +24,30 @@ export default function HomeScreen() {
     }
     
   };
-  
+  console.log('DBuser');
+  console.log(dbUser);
 
   useEffect(() => {
     fetchRestaurants();
   }, []);
+const displayNotifacation = () => {
+  Dialog.show({
+    type: ALERT_TYPE.SUCCESS,
+    title: 'Welcome',
+    textBody: 'Dont forget to input your name and email into the profile screen please.',
+    button: 'Close', 
+  });
+}
+  useEffect(() => {
+    if(!dbUser) {
+      console.log('Effect');
+     displayNotifacation();
+    }
+  }, []);
 
   return (
     <View style={styles.page}>
+
       <FlatList
         data={restaurants}
         renderItem={({ item }) => <RestaurantItem restaurant={item} />}
